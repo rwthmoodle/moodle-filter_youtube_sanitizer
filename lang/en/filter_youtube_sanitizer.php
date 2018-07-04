3675406aa3b84f65dd06b3191dff700553601a8d
eggSmellFart <juanzondonjuan@gmail.com>
2018-07-04 13:28:39 +0200

    added comment block

--- lang/en/filter_youtube_sanitizer.php
+++ lang/en/filter_youtube_sanitizer.php
@@ -1,20 +1,29 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for local_moodlecheck
+ *
+ * @package    filter_youtube_sanitizer
+ * @copyright  2018 Markus Offermann
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 $string['filtername'] = 'Youtube Sanitizer';
 $string['pluginname'] = 'Youtube Sanitizer';
 $string['no-js-message'] = 'Please activate JavaScript to watch the Video';
-// $string['yt-info'] = "the Video will be embedded by Youtube Services. By playing you agree to the ";
 $string['terms'] = 'The Video will be played via YouTube. You agree to';
 $string['conditions'] = 'the terms and conditions of google.';
 $string['url'] = 'https://www.google.com/intl/en/policies/privacy/';
-
-// $string['terms-conditions'] = <<<EOD
-// 	'<div class="overlay">
-// 		<input class="privacy-play-btn" type="button" value="PLAY"></input>
-// 		<div class="small">
-// 			the Video will be played embedded in YouTube. You agree to
-// 			<a href="/var/www/html/moodle_mo/filter/youtube_sanitizer/lang/en/filter_youtube_sanitizer.php" target="_blank">
-// 				terms and conditions of Google
-// 			</a>
-// 		</div>
-// 	</div>'
-// EOD;
