df475295093579a69cb00722e5fc0c6da3bb3438
eggSmellFart <juanzondonjuan@gmail.com>
2018-07-04 13:34:40 +0200

    changes in the comment block->chnaged name!

--- version.php
+++ version.php
@@ -15,9 +15,9 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Version details
+ * Youtube Sanitizer
  *
- * @package    filter
+ * @package    filter_youtube_sanitizer
  * @subpackage tidy
  * @copyright  1999 onwards Martin Dougiamas (http://dougiamas.com)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
