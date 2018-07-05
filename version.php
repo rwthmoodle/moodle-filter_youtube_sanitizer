da01b8471dbedd624d5bb7754ad71158f7856bc3
eggSmellFart <juanzondonjuan@gmail.com>
2018-07-05 13:57:59 +0200

    huma readable version

--- version.php
+++ version.php
@@ -25,6 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018061501;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2017110800;        // Requires this Moodle version
-$plugin->component = 'filter_youtube_sanitizer';     // Full name of the plugin (used for diagnostics)
+$plugin->version   = 2018061501;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2017110800;        // Requires this Moodle version.
+$plugin->component = 'filter_youtube_sanitizer';     // Full name of the plugin (used for diagnostics).
+$plugin->release = '1.0.0'
