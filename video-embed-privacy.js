53c6a1fb09e6183bb924d37894dc3129c8860d80
markusoffermann <offermann@itc.rwth-aachen.de>
2018-07-18 16:39:57 +0200

    ..:

--- video-embed-privacy.js
+++ video-embed-privacy.js
@@ -1,15 +1,6 @@
 require(['jquery'], function($) {
     $(function() {
-        /*$(".video-wrapped").each(function() {
-            $(this).empty().append($('<div class="video-wrapped-play">').html($(this).attr('data-embed-play')));
-			$(this).click(function(e) {
-				$(this).html($(this).attr('data-embed-frame').replace(/(\/embed\/[^"]*\\?[^"]*)/, '$1?autoplay=1'));
-				$(this).find('button').mousedown();
-				$(this).removeAttr('style').unbind("click");
-			});
-        });*/
 		function applyVideoWrapper() {
-
 			$(".video-wrapped").each(function(obj) {
 				$(this).empty().append($('<div class="video-wrapped-play">').html($(this).attr('data-embed-play')));
 				$(this).click(function() {
@@ -34,10 +25,6 @@ require(['jquery'], function($) {
 				});
 			});
 		}
-
 		applyVideoWrapper();
-		/*$('.button-yt').each(function(e) {
-			$(this).StopPropagation();
-		})*/
     });
 });
