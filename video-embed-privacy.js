5d5c7c21b9396afc6f1569a5feee7fcadd43622f
eggSmellFart <juanzondonjuan@gmail.com>
2018-09-25 11:21:30 +0200

    chnages to remove bugs

--- video-embed-privacy.js
+++ video-embed-privacy.js
@@ -1,30 +1,36 @@
 require(['jquery'], function($) {
-    $(function() {
-		function applyVideoWrapper() {
-			$(".video-wrapped").each(function(obj) {
-				$(this).empty().append($('<div class="video-wrapped-play">').html($(this).attr('data-embed-play')));
-				$(this).on('click mousedown', function() {
-					var videoWrapped = $(this);
-					videoWrapped.find('.video-wrapped-play').remove();
-					var videoWrapped = $(this).parents('.video-wrapped');
-
-					$(this).append($(this).attr('data-embed-frame').replace(/(\/embed\/[^"]*\\?[^"]*)/, '$1?autoplay=1'));
-					$(this).find('button').mousedown();
-					$(this).removeAttr('style').unbind("click");
-				});
+	return {
+		init: function() {
+		
+			$(function() {
+				function applyVideoWrapper() {
+					$(".video-wrapped").each(function(obj) {
+						$(this).empty().append($('<div class="video-wrapped-play">').html($(this).attr('data-embed-play')));
+						$(this).on('click mousedown', function() {
+							var videoWrapped = $(this);
+							videoWrapped.find('.video-wrapped-play').remove();
+							var videoWrapped = $(this).parents('.video-wrapped');
+							
+							$(this).append($(this).attr('data-embed-frame').replace(/(\/embed\/[^"]*\\?[^"]*)/, '$1?autoplay=1'));
+							$(this).find('button').mousedown();
+							$(this).removeAttr('style').unbind("click");
+						});
+					});
+					preventBubbling();
+				}
+				function preventBubbling() {
+					$('.yt-link-wrapper a, yt-link-wrapper span').each(function(index) {
+						// console.log(index, ':  ', $(this));
+						// console.log($(this));
+						$(this).on('click mousedown', function(event) {
+							event.stopPropagation();
+							// console.log($(event));
+						});
+					});
+				}
+				applyVideoWrapper();
 			});
-			preventBubbling();
-		}
-		function preventBubbling() {
-			$('.yt-link-wrapper a, yt-link-wrapper span').each(function(index) {
-				// console.log(index, ':  ', $(this));
-				// console.log($(this));
-				$(this).on('click mousedown', function(event) {
-					event.stopPropagation();
-					// console.log($(event));
-				});
-			});
-		}
-		applyVideoWrapper();
-    });
+		}	
+	
+	}
 });
