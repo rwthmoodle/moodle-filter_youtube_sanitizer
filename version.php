d4cd0480b70646b82210601eaaf904ed7593a293
Tim Schroeder <t.schroeder@itc.rwth-aachen.de>
2020-09-15 12:37:17 +0200

    use correct encoding again in html parsing

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020081100;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2020091500;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2017110800;        // Requires this Moodle version.
 $plugin->component = 'filter_youtube_sanitizer';     // Full name of the plugin (used for diagnostics).
 $plugin->release   = '1.1.1';
